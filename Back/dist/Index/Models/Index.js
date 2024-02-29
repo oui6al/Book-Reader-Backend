100814e92fc4d9fa4680e4097051dbca9c8c9653
WAMANR <59700151+WAMANR@users.noreply.github.com>
2024-02-29 15:15:28 +0100

    Delete Back/dist directory

--- Back/dist/Index/Models/Index.js
+++
@@ -1,9 +0,0 @@
-class Index {
-    id;
-    tokens;
-    constructor(id, tokens) {
-        this.id = id;
-        this.tokens = tokens;
-    }
-}
-export default Index;
--- Back/dist/Index/Models/ReverseIndex.js
+++
@@ -1,9 +0,0 @@
-class ReverseIndex {
-    token;
-    books;
-    constructor(token, books) {
-        this.token = token;
-        this.books = books;
-    }
-}
-export default ReverseIndex;
